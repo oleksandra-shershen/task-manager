76004e49aedc749eafd9e36c8b9bca02f807a8d3
Shershen Alexandra <hornet240204@gmail.com>
2024-05-11 19:01:47 +0300

    Add models and make migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from task_manager.models import Worker
+
+
+class Profile(models.Model):
+    worker = models.OneToOneField(Worker, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        super().save()
--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,78 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from task_manager_service import settings
+
+
+class Position(models.Model):
+    POSITION_CHOICES = (
+        ("developer", "Developer"),
+        ("pm", "Project Manager"),
+        ("designer", "Designer"),
+        ("devops", "DevOps"),
+        ("qa", "QA"),
+    )
+
+    name = models.CharField(max_length=255, choices=POSITION_CHOICES)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    TASK_TYPE_CHOICES = (
+        ("Bug", "Bug"),
+        ("New Feature", "New Feature"),
+        ("Refactoring", "Refactoring"),
+        ("QA", "QA"),
+    )
+
+    name = models.CharField(
+        max_length=255, choices=TASK_TYPE_CHOICES, blank=True, null=True
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position, on_delete=models.SET_NULL, null=True, related_name="workers",
+    )
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.get_full_name()})"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("Urgent", "Urgent"),
+        ("Low", "Low"),
+        ("Medium", "Medium"),
+        ("High", "High"),
+    )
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    created_date = models.DateField(auto_now_add=True, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    deadline = models.DateTimeField(
+        auto_now=False, auto_now_add=False, blank=True, null=True
+    )
+    is_completed = models.BooleanField(default=False)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="assigned_tasks")
+
+    class Meta:
+        ordering = ["created_date"]
+
+    def __str__(self):
+        return self.name
