c45874fbddec6ecd866b6b7295be0e28646c2b20
Shershen Alexandra <hornet240204@gmail.com>
2024-05-11 19:53:42 +0300

    Add home view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Task
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
