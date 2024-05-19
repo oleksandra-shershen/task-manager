6c43e0287917bb7ece79d826e4a29e4127c16c08
Shershen Alexandra <hornet240204@gmail.com>
2024-05-19 19:04:41 +0300

    Add field main_programming_language to profile; Add field phone to profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 from task_manager.models import Worker
 
 
 class Profile(models.Model):
     worker = models.OneToOneField(Worker, on_delete=models.CASCADE)
+
+    phone = PhoneNumberField(blank=True, null=True, help_text="Mobile phone number")
+    main_programming_language = models.CharField(max_length=255, default="")
 
     def save(self, *args, **kwargs):
         super().save()
