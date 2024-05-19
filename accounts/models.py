47edec9be0eee770b3a95c1aed93aaafe7458c5d
Shershen Alexandra <hornet240204@gmail.com>
2024-05-19 19:05:58 +0300

    Alter field main_programming_language on profile

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     worker = models.OneToOneField(Worker, on_delete=models.CASCADE)
 
     phone = PhoneNumberField(blank=True, null=True, help_text="Mobile phone number")
-    main_programming_language = models.CharField(max_length=255, default="")
+    main_programming_language = models.CharField(max_length=255, default="Not specified.")
 
     def save(self, *args, **kwargs):
         super().save()
