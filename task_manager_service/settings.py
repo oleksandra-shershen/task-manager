b74f3e6299ae88ef46da7fe1dd5d197f8ed55a40
Shershen Alexandra <hornet240204@gmail.com>
2024-05-21 20:16:47 +0300

    Add ALLOWED_HOSTS

--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['task-manager-l5zb.onrender.com', '127.0.0.1']
+
 
 # Application definition
 
